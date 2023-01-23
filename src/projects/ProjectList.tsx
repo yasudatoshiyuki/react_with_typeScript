f998647f4adc497c478c7582c1d97b7545280fac
Toshiyuki Yasuda <toshi1973814@gmail.com>
2023-01-23 18:20:30 +0900

    Lab10

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Project } from './Project';
 import ProjectCard from "./ProjectCard";
+import ProjectForm from "./ProjectForm";
 
 interface ProjectListProps {
     projects: Project[];
 }
 
 function ProjectList({ projects }: ProjectListProps) {
+    const items = projects.map((project) => (
+        <div key={project.id} className="cols-sm">
+            <ProjectCard project={project} key={project.id} />
+            <ProjectForm />
+        </div>
+    ));
     return (
-        <div className="row">
-            {projects.map((project) => (
-                <ProjectCard project={project} key={project.id} />
-                ))}
-            </div>
+        <div className="row">{items}</div>
     );
 }
 export default ProjectList;
