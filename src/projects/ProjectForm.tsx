590e41f7502a362ae37d8d9e74afa8455c40f78c
Toshiyuki Yasuda <toshi1973814@gmail.com>
2023-01-23 23:16:02 +0900

    Lab15

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -1,26 +1,81 @@
-import React from "react";
-import { Project } from './Project';
+import React, { SyntheticEvent, useState } from "react";
+import { Project } from "./Project";
 
-function ProjectForm() {
-    return (
-        <form className="input-group vertical">
-            <label htmlFor="name">Project Name</label>
-            <input type="text" name="name" placeholder="enter name" />
-            <label htmlFor="description">Project Description</label>
+interface ProjectFormProps {
+  project: Project;
+  onSave: (project: Project) => void;
+  onCancel: () => void;
+}
 
-            <textarea name="description" placeholder="enter description"></textarea>
-            <label htmlFor="budget">Project Budget</label>
+function ProjectForm({
+  project: initialProject,
+  onSave,
+  onCancel,
+}: ProjectFormProps) {
+  const [project, setProject] = useState(initialProject);
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    onSave(project);
+  };
 
-            <input type="number" name="budget" placeholder="enter budget" />
-            <label htmlFor="isActive">Active?</label>
-            <input type="checkbox" name="isActive" />
+  const handleChange = (event: any) => {
+    const { type, name, value, checked } = event.target;
+    let updatedValue = type === "checkbox" ? checked : value;
+    if (type === "number") {
+      updatedValue = Number(updatedValue);
+    }
+    const change = {
+      [name]: updatedValue,
+    };
+    let updatedProject: Project;
+    setProject((p) => {
+      updatedProject = new Project({ ...p, ...change });
+      return updatedProject;
+    });
+  };
 
-            <div className="input-group">
-                <button className="primary bordered medium">Save</button>
-                <span></span>
-                <button type="button" className="bordered medium">cancel</button>
-            </div>
-        </form>
-    );
+  return (
+    <form className="input-group vertical" onSubmit={handleSubmit}>
+      <label htmlFor="name">Project Name</label>
+      <input
+        type="text"
+        name="name"
+        placeholder="enter name"
+        value={project.name}
+        onChange={handleChange}
+      />
+      <label htmlFor="description">Project Description</label>
+
+      <textarea
+        name="description"
+        placeholder="enter description"
+        value={project.description}
+        onChange={handleChange}
+      ></textarea>
+      <label htmlFor="budget">Project Budget</label>
+
+      <input
+        type="number"
+        name="budget"
+        placeholder="enter budget"
+        value={project.budget}
+        onChange={handleChange}
+      />
+      <label htmlFor="isActive">Active?</label>
+      <input
+        type="checkbox"
+        name="isActive"
+        checked={project.isActive}
+        onChange={handleChange}
+      />
+      <div className="input-group">
+        <button className="primary bordered medium">Save</button>
+        <span></span>
+        <button type="button" className="bordered medium" onClick={onCancel}>
+          cancel
+        </button>
+      </div>
+    </form>
+  );
 }
 export default ProjectForm;
--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,28 +1,34 @@
 import React, { useState } from "react";
-import { Project } from './Project';
+import { Project } from "./Project";
 import ProjectCard from "./ProjectCard";
 import ProjectForm from "./ProjectForm";
 
 interface ProjectListProps {
-    projects: Project[];
+  projects: Project[];
+  onSave: (project: Project) => void;
 }
 
-function ProjectList({ projects }: ProjectListProps) {
-    const [projectBeingEdited, setProjectBeingEdited] = useState({});
-    const handleEdit = (project: Project) => {
-        setProjectBeingEdited(project);
-    }
-    const items = projects.map((project) => (
-        <div key={project.id} className="cols-sm">
-            {project===projectBeingEdited ? (
-                <ProjectForm />
-            ) : (
-                <ProjectCard project={project} onEdit= {handleEdit} />
-            )}
-        </div>
-    ));
-    return (
-        <div className="row">{items}</div>
-    );
+function ProjectList({ projects, onSave }: ProjectListProps) {
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
+  const handleEdit = (project: Project) => {
+    setProjectBeingEdited(project);
+  };
+  const cancelEditing = () => {
+    setProjectBeingEdited({});
+  };
+  const items = projects.map((project) => (
+    <div key={project.id} className="cols-sm">
+      {project === projectBeingEdited ? (
+        <ProjectForm
+          project={project}
+          onSave={onSave}
+          onCancel={cancelEditing}
+        />
+      ) : (
+        <ProjectCard project={project} onEdit={handleEdit} />
+      )}
+    </div>
+  ));
+  return <div className="row">{items}</div>;
 }
 export default ProjectList;
