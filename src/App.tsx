604d6ff6589bdaa0cbb408a77595e39cdfed8c5c
Toshiyuki Yasuda <toshi1973814@gmail.com>
2023-01-23 14:37:35 +0900

    Lab4

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Project } from './projects/Project';
+import ProjectsPage from './projects/ProjectsPage';
 
 function App() {
   return (
-    <blockquote cite="Benjamin Franklin">
-      Tell me and I forget, teach me and I may remember, involve me and I learn. 
-    </blockquote>
+    <div className='container'>
+      <ProjectsPage />
+    </div>
   );
 }
 
